e01ac4f9414043c95ba6ef40b86c38756d94934d
JC-ProPlus <guo121guo@gmail.com>
2019-06-02 11:04:05 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -6,37 +6,37 @@ return (new Extend\Formatter)
         ->configure(function (Configurator $config){
             $config->BBCodes->addCustom(
                 '[huaji]',
-                '<img src="./assets/0x.png"/>',
+                '<img src="./vendor/jc-proplus/tieba-stickers/assets/0x.png"/>',
             );
 
             $config->BBCodes->addCustom(
                 '[yinxian]',
-                '<img src="./assets/0p.png"/>',
+                '<img src="./vendor/jc-proplus/tieba-stickers/assets/0p.png"/>',
             );
 
             $config->BBCodes->addCustom(
                 '[pen]',
-                '<img src="./assets/1g.png">',
+                '<img src="./vendor/jc-proplus/tieba-stickers/assets/1g.png">',
             );
 
             $config->BBCodes->addCustom(
                 '[hehe]',
-                '<img src="./assets/0a.png">',
+                '<img src="./vendor/jc-proplus/tieba-stickers/assets/0a.png">',
             );
 
             $config->BBCodes->addCustom(
                 '[cry]',
-                '<img src="./assets/0i.png"/>',
+                '<img src="./vendor/jc-proplus/tieba-stickers/assets/0i.png"/>',
             );
 
             $config->BBCodes->addCustom(
                 '[angry]',
-                '<img src="./assets/0f.png"/>',
+                '<img src="./vendor/jc-proplus/tieba-stickers/assets/0f.png"/>',
             );
 
             $config->BBCodes->addCustom(
                 '[smirking]',
-                '<img src="./assets/1i.png"/>',
+                '<img src="./vendor/jc-proplus/tieba-stickers/assets/1i.png"/>',
             );
 
         });
