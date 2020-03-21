f40da7ff7978b799bb79eecf1052ee0e48c66869
JC_ProPlus <guo121guo@gmail.com>
2020-03-21 12:50:32 +0800

    Image size change

--- extend.php
+++ extend.php
@@ -6,6 +6,6 @@ return (new Extend\Formatter)
         ->configure(function (Configurator $config){
             $config->BBCodes->addCustom(
                 '[tieba={TEXT1}]',
-                '<img src="https://img.wvbforum.xyz/tieba/{TEXT1}.png"/>',
+                '<img src="https://img.wvbforum.xyz/tieba/{TEXT1}.png" width="30px"/>',
             );
         });
