c128fc079448732d80a83533bd9535b3b31a1643
JC_ProPlus <jcproplus@wvb.xyz>
2020-08-15 16:06:37 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -6,6 +6,6 @@ return (new Extend\Formatter)
         ->configure(function (Configurator $config){
             $config->BBCodes->addCustom(
                 '[tieba={TEXT1}]',
-                '<img src="https://img.wvbforum.xyz/tieba/{TEXT1}.png" width="30px"/>',
+                '<img class="tieba_stickers" src="https://community.wvbtech.com/assets/tieba/{TEXT1}.png" width="30px"/>',
             );
         });
